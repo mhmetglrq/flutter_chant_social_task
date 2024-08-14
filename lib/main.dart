a3e871601b6dc6bc940f14ca9b7881f400cf34c4
mhmetglrq <mhmetglr.q@gmail.com>
2024-08-14 17:12:52 +0300

    feat: Save user's name in userSettings box during onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chant_social_task/firebase_options.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'my_app.dart';
 
 void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('userSettings');
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
