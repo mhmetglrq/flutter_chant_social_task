318d7c485462e0e2bfe72f3cce84162427f0b052
mhmetglrq <mhmetglr.q@gmail.com>
2024-08-14 16:54:30 +0300

    chore: Initialize Firebase in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chant_social_task/firebase_options.dart';
 
 import 'my_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
